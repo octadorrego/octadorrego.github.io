3e6393def12ca7b68af54a689894a126f31eb701
octadorrego <139635326+octadorrego@users.noreply.github.com>
2024-04-29 20:31:47 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,49 @@
-const boton1=document.getElementById('boton1');
-const boton2=document.getElementById('boton2');
-const body=document.querySelector('body');
-let sound = new Audio('jeff.mp4');
+const boton = document.getElementById("boton");
+const titulo = document.getElementById("titulo");
+const songcont = document.getElementById("song-cont")
 
-const a=document.getElementById('cookies');
-const b=document.getElementById('this');
+boton.addEventListener('click', () =>{
+    datoscanciones();
+})
 
-boton1.addEventListener('click', function(){
-    body.classList.remove("body");
-    body.classList.add("body2");
-    sound.play();
-    a.style.display = "none";
-    b.style.display = "none";
-    boton1.style.display = "none";
-    boton2.style.display = "none";
+async function datoscanciones(){
+    const url = `https://shazam.p.rapidapi.com/auto-complete?term=${titulo.value}&locale=en-US`;
+    const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '20551f460fmsh9934765550508cdp1666ffjsn045b7373051c',
+		'X-RapidAPI-Host': 'shazam.p.rapidapi.com',
+	},
+};
 
-
-});
-
-boton2.addEventListener('click', function(){
-    body.classList.remove("body");
-    body.classList.add("body2");
-    sound.play();
-    a.style.display = "none";
-    b.style.display = "none";
-    boton1.style.display = "none";
-    boton2.style.display = "none";
-});
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        songcont.classList.remove("song-cont2");
+        songcont.classList.add("songcont");
+        songcont.innerHTML = `
+        
+        
+            <p class="canciones">${result.hints[0].term}</p><br>
+            <p class="canciones">${result.hints[1].term}</p><br>
+            <p class="canciones">${result.hints[2].term}</p><br>
+            <p class="canciones">${result.hints[3].term}</p><br>
+            <p class="canciones">${result.hints[4].term}</p><br>
+            <p class="canciones">${result.hints[5].term}</p><br>
+            <p class="canciones">${result.hints[6].term}</p><br>
+            <p class="canciones">${result.hints[7].term}</p><br>
+            <p class="canciones">${result.hints[8].term}</p><br>
+            <p class="canciones">${result.hints[9].term}</p><br>
+        
+        
+        
+        
+        
+        
+        
+        `
+        //console.log(result.hints[0].term);
+    } catch (error) {
+        console.error(error);
+    }
+}
